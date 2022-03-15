da91aed049a8a03098898d5e15db99c87ed0956a
B Raci <basia@live.ca>
2022-03-15 12:23:17 -0600

    fixed axios issue - fixed link to script file

--- src/script.js
+++ src/script.js
@@ -6,7 +6,8 @@
 function getTemperature(response) {
   console.log(response);
 }
-let apiKey = "01a738ffcc406d9b10304ab407495deb";
+let apiKey = `01a738ffcc406d9b10304ab407495deb`;
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
+console.log("tye");
 axios.get(apiUrl).then(getTemperature);

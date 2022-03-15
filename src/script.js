6d71fac087ff6262d7d9e2a63d5bb5d9cf176b1b
B Raci <basia@live.ca>
2022-03-15 17:14:34 -0600

    date & time on submit

--- src/script.js
+++ src/script.js
@@ -4,6 +4,50 @@
 // change city
 
 // change temp
+function dateTime(timestamp) {
+
+  let daysWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ]
+
+  let months = [
+    "Jan",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+    
+  ]
+
+
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let day = daysWeek[date.getDay()];
+  let dateToday = date.getDate();
+  let month = months[date.getMonth()];
+  let year =date.getFullYear();
+  if (minutes<10) {
+    minutes = `0${minutes}`}
+  if (hours<10) {
+    hours = `0${hours}`
+  }
+  
+  
+  return `${day}, ${month} ${dateToday}, ${year} 🌎 ${hours}:${minutes}`;
+}
 
 function getTemperature(response) {
   let currentTemp = document.querySelector("#temp");
@@ -12,15 +56,17 @@ function getTemperature(response) {
   let description = document.querySelector("#description");
   let humidity = document.querySelector("#humidity");
   let windSpeed = document.querySelector("#wind-speed");
+  let currentDateTime = document.querySelector("#date");
 
   currentTemp.innerHTML = Math.round(response.data.main.temp);
   currentCity.innerHTML = `${response.data.name}`;
-  country.innerHTML = `${response.data.sys.country}`
+  country.innerHTML = `${response.data.sys.country}`;
   description.innerHTML = `${response.data.weather[0].main}`;
   humidity.innerHTML = `${response.data.main.humidity}`;
   windSpeed.innerHTML = `${Math.round(response.data.wind.speed)}`;
+  currentDateTime.innerHTML = dateTime(response.data.dt * 1000);
 
-console.log(response);
+  console.log(response);
 }
 
 // change city
@@ -30,7 +76,7 @@ function handleSubmit(event) {
   let cityInputElement = document.querySelector("#city-input");
   let cityText = document.querySelector("#city");
 
-//
+  //
   let apiKey = `01a738ffcc406d9b10304ab407495deb`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInputElement.value}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(getTemperature);

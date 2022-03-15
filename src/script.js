449fe321a0672bfe52883254dbf346f5d86dd614
B Raci <basia@live.ca>
2022-03-15 01:23:08 -0600

    axios call - issue

--- src/script.js
+++ src/script.js
@@ -1,1 +1,12 @@
-// 
+// api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+//01a738ffcc406d9b10304ab407495deb
+
+// select city
+
+function getTemperature(response) {
+  console.log(response);
+}
+let apiKey = "01a738ffcc406d9b10304ab407495deb";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(getTemperature);
